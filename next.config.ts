605176f87626c58b9fa51354b6901ba4e793450e
Raphael Freitas <o.raphaelfreitas@gmail.com>
2025-07-15 18:57:19 -0300

    block iframe rendering

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,23 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: [
     "http://localhost:3000",
     "https://panda-calm-really.ngrok-free.app/",
-    "*.ngrok-free.app"
+    "*.ngrok-free.app",
   ],
   images: {
-    remotePatterns: [new URL('https://*/**')],
+    remotePatterns: [new URL("https://*/**")],
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY", // or 'SAMEORIGIN' if you want to allow same-origin iframes
+          },
+        ],
+      },
+    ];
   },
 };
 
